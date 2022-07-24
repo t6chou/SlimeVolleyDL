8c2a5767248a5974dbb043fb68bbb7719756e482
Ayan Khan <am37khan@uwaterloo.ca>
2022-07-23 21:57:01 -0400

    init branch

--- mazeworld/eval_maze.py
+++
@@ -1,6 +0,0 @@
-from stable_baselines.common.env_checker import check_env
-from maze_env import Maze
-
-env = Maze()
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
